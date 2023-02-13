1c2cc987ff16d6ec26c41de66d5fa342f683177e
guillermo <h.uhm292@gmail.com>
2023-02-13 09:47:18 +0100

    first commit

---
+++ src/main/java/com/factoriaf5/clinicadental/ClinicadentalApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.clinicadental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicadentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicadentalApplication.class, args);
+	}
+
+}
