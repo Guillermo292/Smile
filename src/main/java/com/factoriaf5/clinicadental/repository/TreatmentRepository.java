80ef66ba69513dc208f2102bf5fc3c40066a8df5
guillermo <h.uhm292@gmail.com>
2023-02-28 10:27:17 +0100

    Pull request

--- src/main/java/com/factoriaf5/clinicadental/repository/TreatmentRepository.java
+++ src/main/java/com/factoriaf5/clinicadental/repository/TreatmentRepository.java
@@ -3,6 +3,6 @@ package com.factoriaf5.clinicadental.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.factoriaf5.clinicadental.models.Treatment;
 
-public class TreatmentRepository {
+public interface TreatmentRepository extends JpaRepository<Treatment, Long> {
     
 }
